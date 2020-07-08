fb4cebe57abf867002cc5030f9fa45e93f897314
Eric <rty83924@gmail.com>
2020-07-08 11:38:46 +0800

    update wmgit config.py

---
+++ wmgit/config.py
@@ -0,0 +1,10 @@
+class config:
+    user = 'user'
+    passwd = 'passwd'
+    domain = 'domain'
+    #工作目錄
+    worktree = 'worktree'
+    #git目錄
+    gitpath = 'gitpath'
+    #rsync shell config
+    conf_path = 'conf_path'
