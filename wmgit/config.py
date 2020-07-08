4eeb79896c6b494df74b2511cd3d2d21339144a7
Eric <rty83924@gmail.com>
2020-07-08 11:35:01 +0800

    update wmgit to config.py

--- wmgit/config.py
+++
@@ -1,7 +0,0 @@
-class config:
-    user = 'a168-rp'
-    passwd = 'XapfHLP4'
-    domain = 'igofun.net:30000'
-    worktree = '/home/work/test'
-    gitpath = '/home/work/test2'
-    conf_path = '/home/work'
